c3009b85294f9400cbd4c9b95aa76f1d783e8ecc
Cristian Thompson <Thompsonacristian@gmail.com>
2021-04-27 01:42:32 -0500

    Add basic movement and state updating

--- ElevatorControlSystem.py
+++ ElevatorControlSystem.py
@@ -1,2 +1,31 @@
-def testFunction():
-    return 'I\'m here.'
+class Elevator:
+    # Declare initial values
+    current_level = 0
+    maximum_level = 10
+    levels = []
+
+    def __init__(self, initial_configuration = None):
+        if initial_configuration is not None:
+            self.current_level = initial_configuration['current_level'] if 'current_level' in initial_configuration else 0
+            self.maximum_level = initial_configuration['maximum_level'] if 'maximum_level' in initial_configuration else 10
+
+    # Tempting as it may be don't remove me, I'm proof tests are running and passing
+    def testFunction(self):
+        return 'I\'m here.'
+
+    def press_buttons(self, buttons_pressed):
+        if not isinstance(buttons_pressed, list):
+            buttons_pressed = [buttons_pressed]
+        self.levels = buttons_pressed + self.levels
+        return self.levels
+    
+    def move(self):
+        # If we aren't on the level declared in our stored levels let's move to that next level
+        if len(self.levels) > 0 and self.current_level != self.levels[0]:
+            self.current_level = self.levels[0]
+            self.levels.pop(0)
+        else:
+            return false
+
+    def report(self):
+        return { 'current_level': self.current_level, 'maximum_level': self.maximum_level, 'levels': self.levels }
--- ElevatorControlSystem_Tests.py
+++ ElevatorControlSystem_Tests.py
@@ -1,10 +1,29 @@
 import unittest
 import ElevatorControlSystem
 
-class TestStringMethods(unittest.TestCase):
+class ElevatorTests(unittest.TestCase):
 
     def test_im_here(self):
-        self.assertEqual(ElevatorControlSystem.testFunction(), 'I\'m here.')
+        elevator = ElevatorControlSystem.Elevator()
+        testFunctionMessage = elevator.testFunction()
+        self.assertEqual(testFunctionMessage, 'I\'m here.')
+    
+    def test_elevator_returns_levels_after_button_press(self):
+        elevator = ElevatorControlSystem.Elevator({"current_level": 0})
+        self.assertEqual(elevator.press_buttons([1]), [1])
+        self.assertEqual(elevator.press_buttons([6]), [6, 1])
+        self.assertEqual(elevator.press_buttons([4]), [4, 6, 1])
+
+    def test_elevator_moves(self):
+        elevator = ElevatorControlSystem.Elevator({"current_level": 0})
+        self.assertEqual(elevator.press_buttons([1]), [1])
+        # Tell the elevator to move
+        elevator.move()
+        self.assertEqual(elevator.report(), {
+            'current_level': 1,
+            'maximum_level': 10,
+            'levels': []
+        })
 
 if __name__ == '__main__':
     unittest.main()
